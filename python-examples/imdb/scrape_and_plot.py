7090226c336109bb04a863951e5ec14b3a1887b2
Seppe vanden Broucke <macuyiko@gmail.com>
2018-02-12 13:26:28 +0100

    fix imdb example

--- python-examples/imdb/scrape_and_plot.py
+++ python-examples/imdb/scrape_and_plot.py
@@ -1,25 +1,23 @@
-import pickle
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://www.imdb.com/title/tt0944947/epdate'
+url = 'http://www.imdb.com/title/tt0944947/episodes'
 
 episodes = []
 ratings = []
 
-soup = BeautifulSoup(requests.get(url).text, 'html.parser')
-table = soup.find(id='tn15content').find('table')
-for row in table.find_all('tr'):
-    cols = row.find_all('td')
-    if not cols:
-        continue
-    episode = cols[0].get_text(strip=True)
-    if episode == '#':
-        # This is the header row
-        continue
-    rating = float(row.find(class_='rating-rating').find(class_='value').get_text(strip=True))
-    episodes.append(episode)
-    ratings.append(rating)
+# Go over seasons 1 to 7
+for season in range(1, 8):
+    r = requests.get(url, params={'season': season})
+    soup = BeautifulSoup(r.text, 'html.parser')
+    listing = soup.find('div', class_='eplist')
+    for epnr, div in enumerate(listing.find_all('div', recursive=False)):
+        episode = "{}.{}".format(season, epnr + 1)
+        rating_el = div.find(class_='ipl-rating-star__rating')
+        rating = float(rating_el.get_text(strip=True))
+        print('Episode:', episode, '-- rating:', rating)
+        episodes.append(episode)
+        ratings.append(rating)
 
 import matplotlib.pyplot as plt
 
